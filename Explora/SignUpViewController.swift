3349a686c3bc9c11f2fa9120ebde0d6585117ead
Matt Mo <matthew.l.mo@gmail.com>
2015-10-18 04:32:36 -0700

    [Sign up] - set up very basic UI and testing functionality

--- Explora/SignUpViewController.swift
+++ Explora/SignUpViewController.swift
@@ -7,20 +7,61 @@
 //
 
 import UIKit
+import Parse
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-        
+        testGetUser()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func submitButtonPressed(sender: AnyObject) {
+        let user = PFUser()
+        user.username = self.emailField.text
+        user.password = self.passwordField.text
+        user.email = self.emailField.text
+        
+        // other fields can be set just like with PFObject
+        user.createdEvents = ["blahblah", "meh"]
+        user.participatingEvents = ["testing", "hah"]
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo["error"] as? NSString
+                // Show the errorString somewhere and let the user try again.
+                print (errorString)
+            } else {
+                // Hooray! Let them use the app now.
+                print ("success signing up")
+            }
+        }
+    }
+    
+    func testGetUser() {
+        PFUser.logInWithUsernameInBackground("blahblah@blah.com", password:"testing") {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                // Do stuff after successful login.
+                print(user)
+            } else {
+                // The login failed. Check error to see why.
+                print(error)
+            }
+        }
     }
     
     func testExploraEvent() {
