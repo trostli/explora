74c7742172cb050ecb1b18a422f8bd2a48e2e340
Matt Mo <matthew.l.mo@gmail.com>
2015-10-18 04:11:05 -0700

    [Explora Event] - add model and register subclass

--- Explora/AppDelegate.swift
+++ Explora/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        ExploraEvent.registerSubclass()
         Parse.setApplicationId(ParseCredentials.defaultCredentials.parseID, clientKey: ParseCredentials.defaultCredentials.parseKey)
         
         return true
--- Explora/ExploraEvent.swift
+++ Explora/ExploraEvent.swift
@@ -1,13 +1,56 @@
 //
-//  ExploraEvents.swift
+//  ExploraEvent.swift
 //  Explora
 //
 //  Created by admin on 10/18/15.
 //  Copyright © 2015 explora-codepath. All rights reserved.
 //
+/*
+    Events
+    - creator id
+    - attendees array of ids
+    - attendees limit?
+    - title
+    - description/tagline
+    - event location
+    - meeting location
+    - meeting start time
+    - meeting end time?
+    - event category
+*/
 
 import Parse
 
-class ExploraEvents: PFObject {
+class ExploraEvent: PFObject, PFSubclassing {
     
+    var creatorID: NSString? {
+        get {
+            return self["creator_id"] as? NSString
+        }
+        set {
+            self["creator_id"] = newValue
+        }
+    }
+    
+    var eventTitle: NSString? {
+        get {
+            return self["event_title"] as? NSString
+        }
+        set {
+            self["event_title"] = newValue
+        }
+    }
+    
+    override class func initialize() {
+        struct Static {
+            static var onceToken : dispatch_once_t = 0;
+        }
+        dispatch_once(&Static.onceToken) {
+            self.registerSubclass()
+        }
+    }
+
+    static func parseClassName() -> String {
+        return "ExploraEvent"
+    }
 }
