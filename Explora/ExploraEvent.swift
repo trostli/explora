02a51d8350fab6d7edc0927493d0ac8c20abd356
Matt Mo <matthew.l.mo@gmail.com>
2015-10-18 04:10:25 -0700

    [Models & api & signup] - add mostly empty files

---
+++ Explora/ExploraEvent.swift
@@ -0,0 +1,13 @@
+//
+//  ExploraEvents.swift
+//  Explora
+//
+//  Created by admin on 10/18/15.
+//  Copyright © 2015 explora-codepath. All rights reserved.
+//
+
+import Parse
+
+class ExploraEvents: PFObject {
+    
+}
---
+++ Explora/ExploraUser.swift
@@ -0,0 +1,9 @@
+//
+//  ExploraUser.swift
+//  Explora
+//
+//  Created by admin on 10/18/15.
+//  Copyright © 2015 explora-codepath. All rights reserved.
+//
+
+import Foundation
