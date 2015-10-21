9c15cde557f7a9199b2641500b4a759b56f2f62e
Daniel Trostli <daniel.trostli@gmail.com>
2015-10-20 22:52:41 -0700

    show user location in map

--- Explora/DiscoveryViewController.swift
+++ Explora/DiscoveryViewController.swift
@@ -8,24 +8,26 @@
 
 import UIKit
 import Mapbox
+import INTULocationManager
 
 class DiscoveryViewController: UIViewController {
 
     @IBOutlet weak var mapView: MGLMapView!
     
+    let locationManager = INTULocationManager.sharedInstance()
+    var userCoordinates: CLLocationCoordinate2D?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("\(mapView.bundledStyleURLs)")
         // initialize the map view
         let styleURL = NSURL(string: "asset://styles/light-v8.json")
         mapView = MGLMapView(frame: view.bounds, styleURL: styleURL)
-        
-        // set the map's center coordinate
-        mapView.setCenterCoordinate(CLLocationCoordinate2D(latitude: 38.894368,
-            longitude: -77.036487),
-            zoomLevel: 15, animated: false)
+        mapView.showsUserLocation = true
+
+        getCurrentLocation()
         view.addSubview(mapView)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,7 +35,27 @@ class DiscoveryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // Should be move outside of controller to user model
+    func getCurrentLocation() {
+        locationManager.requestLocationWithDesiredAccuracy(INTULocationAccuracy.Room, timeout: 10.0, delayUntilAuthorized: true) { (currentLocation: CLLocation!, achievedAccuracy: INTULocationAccuracy, status: INTULocationStatus) -> Void in
+            if (status == INTULocationStatus.Success) {
+                self.userCoordinates = currentLocation.coordinate
+                self.mapView.setCenterCoordinate(CLLocationCoordinate2D(latitude: currentLocation.coordinate.latitude,
+                    longitude: currentLocation.coordinate.longitude),
+                    zoomLevel: 15, animated: false)
+            }
+            else if (status == INTULocationStatus.TimedOut) {
+                // Wasn't able to locate the user with the requested accuracy within the timeout interval.
+                // However, currentLocation contains the best location available (if any) as of right now,
+                // and achievedAccuracy has info on the accuracy/recency of the location in currentLocation.
+            }
+            else {
+                // An error occurred, more info is available by looking at the specific status returned.
+            }
+        }
 
+    }
+    
     /*
     // MARK: - Navigation
 
